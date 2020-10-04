db6c04fb15d7e1cfd01ccdd97bb63d8b88f194cb
Mark <01markhuynh@gmail.com>
2020-10-04 01:59:38 -0700

    Updates

---
+++ precache-manifest.8f74c5ebe664b5c445bcff2a9d5460f7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed1760a6aa124fc8efe676addf61163",
+    "url": "/EthicalNLP-FrontEnd/index.html"
+  },
+  {
+    "revision": "69bbc49ff96082d6a854",
+    "url": "/EthicalNLP-FrontEnd/static/css/2.9f5838fa.chunk.css"
+  },
+  {
+    "revision": "16adf9f08bd04ba19973",
+    "url": "/EthicalNLP-FrontEnd/static/css/main.f05143ed.chunk.css"
+  },
+  {
+    "revision": "69bbc49ff96082d6a854",
+    "url": "/EthicalNLP-FrontEnd/static/js/2.e78805cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/EthicalNLP-FrontEnd/static/js/2.e78805cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16adf9f08bd04ba19973",
+    "url": "/EthicalNLP-FrontEnd/static/js/main.89f9a4d9.chunk.js"
+  },
+  {
+    "revision": "3b7fb970a7bc3c104225",
+    "url": "/EthicalNLP-FrontEnd/static/js/runtime-main.04061c8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EthicalNLP-FrontEnd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/EthicalNLP-FrontEnd/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
